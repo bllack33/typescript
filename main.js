4415ba00203cfd181fc3fd0c19c7cf85b09e7d36
bllack33 <bllack33@gmail.com>
2020-05-07 16:34:42 -0500

    20200507

--- main.js
+++ main.js
@@ -2,3 +2,13 @@
 var esSuperman = true;
 var esBatman;
 var esAcuaman = true;
+
+var settings = {
+    "url": "https://www.amazon.com/Retevis-Talkies-Scrambler-Acoustic-Earpiece/dp/B00LHRKG72",
+    "method": "GET",
+    "timeout": 0,
+  };
+  
+  ajax(settings).done(function (response) {
+    console.log(response);
+  });
